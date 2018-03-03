f02e85558bee089e510290c8a405fe8683f57d1a
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2018-03-03 18:40:12 +0100

    Fetching enrolled users works.

--- src/app/services/enrollment.service.ts
+++ src/app/services/enrollment.service.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
 import {UserService} from './user.service';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class EnrollmentService {
 
   private enrollmentsUrl = 'api/enrollments';
 
-  constructor(private http: HttpClient, private userService: UserService) { }
+  constructor(private http: HttpClient, private userService: UserService) {
+  }
 
   getUsersEnrolled(trainingId: number): Observable<any[]> {
     return this.http.get<any[]>(`${this.enrollmentsUrl}?trainingId=${trainingId}`)
       .flatMap(enrollments => {
+        if (!enrollments.length) return Observable.of([]);
         return Observable.forkJoin(
           enrollments.map(enrollment => {
             return this.userService.getUser(enrollment.userId);
