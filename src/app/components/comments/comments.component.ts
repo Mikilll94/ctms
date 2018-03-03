c5937f01f4ec22186f3a46c97a70351d859043e7
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2018-03-03 18:55:11 +0100

    Another small refactoring

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -23,8 +23,6 @@ export class CommentsComponent implements OnInit {
     this.commentAdded = true;
     const id = +this.route.snapshot.paramMap.get('id');
     this.commentService.addComment(new Comment('Gall Anonim', id, content, new Date(Date.now())))
-      .subscribe(comment => {
-        this.comments.unshift(comment);
-      });
+      .subscribe(comment => this.comments.unshift(comment));
   }
 }
